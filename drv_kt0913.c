5ad5e6c85e86e826a13fea29540f4e2d3ba8ba28
Chimipupu <chiminyanyan@gmail.com>
2026-04-29 22:54:42 +0900

    ドライバの実装ベース

---
+++ drv_kt0913.c
@@ -0,0 +1,30 @@
+/**
+ * @file drv_kt0913.c
+ * @author Chimipupu(https://github.com/Chimipupu)
+ * @brief DSPラジオIC KT0913 ドライバ
+ * @version 0.1
+ * @date 2026-04-29
+ * @copyright Copyright (c) 2026 Chimipupu All Rights Reserved.
+ */
+
+#include "drv_kt0913.h"
+
+// -----------------------------------------------------------
+
+// -----------------------------------------------------------
+// [API]
+
+void drv_kt0913_init(kt0913_config_t *p_config)
+{
+    // TODO: 実装
+}
+
+void drv_kt0913_set_reg(uint8_t reg_addr, uint8_t reg_val)
+{
+    // TODO: 実装
+}
+
+void drv_kt0913_mute_onoff(bool is_mute)
+{
+    // TODO: 実装
+}
